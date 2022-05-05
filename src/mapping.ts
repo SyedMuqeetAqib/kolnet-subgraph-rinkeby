3fa4f938fbabf467ea96610d1c4c3c0e7fa54495
SyedMuqeetAqib <smuqeetaqib@gmail.com>
2022-05-05 22:01:02 +0500

    add user role

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
   protoKol,
   CampaignCreated,
@@ -12,34 +12,91 @@ import {
   SetMaxTGEAllowance,
   SetPenalty,
   StakingPercentageUpdated,
-  TGEDeposited
-} from "../generated/protoKol/protoKol"
-import { ExampleEntity } from "../generated/schema"
+  TGEDeposited,
+} from "../generated/protoKol/protoKol";
+import {
+  Campaign,
+  Kol,
+  KolInvestmentTransaction,
+  User,
+} from "../generated/schema";
 
 export function handleCampaignCreated(event: CampaignCreated): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let campaign = Campaign.load(event.params._campaign.campaignNumber.toHex());
+  if (!campaign) {
+    campaign = new Campaign(event.params._campaign.campaignNumber.toHex());
+    campaign.CampaignId = event.params._campaign.campaignNumber;
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    campaign.InvestmentClaimed =
+      event.params._campaign.campaignData.investmentClaimed;
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    campaign.PreSaleTokenBalance = new BigInt(0);
+
+    campaign.ClaimBackInvestment =
+      event.params._campaign.campaignData.claimBackInvestment;
+
+    campaign.PreSaleToken = event.params._campaign.campaignData.preSaleToken;
+
+    campaign.CampaignOwner = event.params._campaign.campaignData.campaignOwner;
+
+    campaign.SecondOwner = event.params._campaign.campaignData.secondOwner;
+
+    campaign.RequiredInvestment =
+      event.params._campaign.campaignData.requiredInvestment;
+
+    campaign.MarketingBudget =
+      event.params._campaign.campaignData.marketingBudget;
+
+    campaign.StartTime = event.params._campaign.campaignData.startTime;
+
+    campaign.EndTime = event.params._campaign.campaignData.endTime;
+
+    campaign.RemainingInvestment =
+      event.params._campaign.campaignData.remainingInvestment;
+
+    campaign.MaxInvest = event.params._campaign.campaignData.maxInvest;
+
+    campaign.NumberOfPostsReq =
+      event.params._campaign.campaignData.numberOfPostsReq;
+
+    campaign.StakingAmount = event.params._campaign.campaignData.stakingAmount;
+
+    campaign.IpfsHash = event.params._campaign.campaignData.ipfsHash;
+
+    campaign.IsVestingInEnabled =
+      event.params._campaign.vestingData.isVestingInEnabled;
+
+    campaign.NumberOfvestings =
+      event.params._campaign.vestingData.NumberOfvestings;
+
+    campaign.VestingCycleDuration =
+      event.params._campaign.vestingData.vestingCycleDuration;
+
+    campaign.VestingAmtPerCycle =
+      event.params._campaign.vestingData.vestingAmtPerCycle;
+
+    campaign.IsTGE = event.params._campaign.tgeDetails.isTGE;
+
+    campaign.TgePercentage = BigInt.fromI32(
+      event.params._campaign.tgeDetails.tgePercentage
+    );
+
+    campaign.TgeTime = event.params._campaign.tgeDetails.tgeTime;
+
+    campaign.TgeAmount = event.params._campaign.tgeDetails.tgeAmount;
+
+    campaign.save();
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity._campaign_campaignNumber = event.params._campaign.campaignNumber
-  entity._campaign_campaignData_investmentClaimed =
-    event.params._campaign.campaignData.investmentClaimed
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
+  let user = User.load(
+    event.params._campaign.campaignData.campaignOwner.toHex()
+  );
+  if (!user) {
+    user = new User(event.params._campaign.campaignData.campaignOwner.toHex());
+    user.WalletAddress = event.params._campaign.campaignData.campaignOwner;
+    user.Role = "project";
+    user.save();
+  }
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
@@ -82,7 +139,74 @@ export function handleCampaignCreated(event: CampaignCreated): void {
 
 export function handleCampaignDetailsUpdated(
   event: CampaignDetailsUpdated
-): void {}
+): void {
+  let campaign = Campaign.load(
+    event.params._newCampaign.campaignNumber.toHex()
+  );
+  if (campaign) {
+    campaign.CampaignId = event.params._newCampaign.campaignNumber;
+
+    campaign.InvestmentClaimed =
+      event.params._newCampaign.campaignData.investmentClaimed;
+
+    campaign.ClaimBackInvestment =
+      event.params._newCampaign.campaignData.claimBackInvestment;
+
+    campaign.PreSaleToken = event.params._newCampaign.campaignData.preSaleToken;
+
+    campaign.CampaignOwner =
+      event.params._newCampaign.campaignData.campaignOwner;
+
+    campaign.SecondOwner = event.params._newCampaign.campaignData.secondOwner;
+
+    campaign.RequiredInvestment =
+      event.params._newCampaign.campaignData.requiredInvestment;
+
+    campaign.MarketingBudget =
+      event.params._newCampaign.campaignData.marketingBudget;
+
+    campaign.StartTime = event.params._newCampaign.campaignData.startTime;
+
+    campaign.EndTime = event.params._newCampaign.campaignData.endTime;
+
+    campaign.RemainingInvestment =
+      event.params._newCampaign.campaignData.remainingInvestment;
+
+    campaign.MaxInvest = event.params._newCampaign.campaignData.maxInvest;
+
+    campaign.NumberOfPostsReq =
+      event.params._newCampaign.campaignData.numberOfPostsReq;
+
+    campaign.StakingAmount =
+      event.params._newCampaign.campaignData.stakingAmount;
+
+    campaign.IpfsHash = event.params._newCampaign.campaignData.ipfsHash;
+
+    campaign.IsVestingInEnabled =
+      event.params._newCampaign.vestingData.isVestingInEnabled;
+
+    campaign.NumberOfvestings =
+      event.params._newCampaign.vestingData.NumberOfvestings;
+
+    campaign.VestingCycleDuration =
+      event.params._newCampaign.vestingData.vestingCycleDuration;
+
+    campaign.VestingAmtPerCycle =
+      event.params._newCampaign.vestingData.vestingAmtPerCycle;
+
+    campaign.IsTGE = event.params._newCampaign.tgeDetails.isTGE;
+
+    campaign.TgePercentage = BigInt.fromI32(
+      event.params._newCampaign.tgeDetails.tgePercentage
+    );
+
+    campaign.TgeTime = event.params._newCampaign.tgeDetails.tgeTime;
+
+    campaign.TgeAmount = event.params._newCampaign.tgeDetails.tgeAmount;
+
+    campaign.save();
+  }
+}
 
 export function handleClaimBackInvestment(event: ClaimBackInvestment): void {}
 
@@ -90,11 +214,79 @@ export function handleClaimKolInvestment(event: ClaimKolInvestment): void {}
 
 export function handleClaimPreSaleTokens(event: ClaimPreSaleTokens): void {}
 
-export function handleDepositPreSaleTokens(event: DepositPreSaleTokens): void {}
+export function handleDepositPreSaleTokens(event: DepositPreSaleTokens): void {
+  let campaign = Campaign.load(event.params.campaign_Id.toHex());
+  if (!campaign) {
+    campaign = new Campaign(event.params.campaign_Id.toHex());
+  }
 
-export function handleInvestInCampaign(event: InvestInCampaign): void {}
+  campaign.PreSaleTokenBalance = campaign.PreSaleTokenBalance.plus(
+    event.params._amount
+  );
 
-export function handleKOLAdded(event: KOLAdded): void {}
+  campaign.save();
+}
+
+export function handleInvestInCampaign(event: InvestInCampaign): void {
+  let kol = Kol.load(event.params._kol.toHex());
+
+  if (!kol) {
+    kol = new Kol(event.params._kol.toHex());
+  }
+
+  let campaign = Campaign.load(event.params.campaign_Id.toHex());
+  if (!campaign) {
+    campaign = new Campaign(event.params.campaign_Id.toHex());
+  }
+
+  let kolInvestment = new KolInvestmentTransaction(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+
+  kolInvestment.CampaignId = event.params.campaign_Id;
+
+  kolInvestment.KolAddress = event.params._kol;
+
+  kol.TotalInvestedAmount = kol.TotalInvestedAmount.plus(event.params._amount);
+
+  kolInvestment.InvestmentShare = event.params._investmentShare;
+
+  kolInvestment.Amount = event.params._amount;
+
+  campaign.Kols = kol.id;
+
+  kolInvestment.Kols = kol.id;
+
+  campaign.save();
+  kol.save();
+  kolInvestment.save();
+}
+
+export function handleKOLAdded(event: KOLAdded): void {
+  let kol = Kol.load(event.params._kol.kolWallet.toHex());
+  if (!kol) {
+    kol = new Kol(event.params._kol.kolWallet.toHex());
+
+    kol.KolId = event.params._kol.kolID;
+
+    kol.KolName = event.params._kol.name;
+
+    kol.KolWalletAddress = event.params._kol.kolWallet;
+
+    kol.TotalInvestedAmount = new BigInt(0);
+
+    kol.IpfsHash = event.params._kol.ipfsHash;
+
+    kol.save();
+  }
+  let user = User.load(event.params._kol.kolWallet.toHex());
+  if (!user) {
+    user = new User(event.params._kol.kolWallet.toHex());
+    user.WalletAddress = event.params._kol.kolWallet;
+    user.Role = "kol";
+    user.save();
+  }
+}
 
 export function handleSetMaxTGEAllowance(event: SetMaxTGEAllowance): void {}
 
@@ -104,4 +296,12 @@ export function handleStakingPercentageUpdated(
   event: StakingPercentageUpdated
 ): void {}
 
-export function handleTGEDeposited(event: TGEDeposited): void {}
+export function handleTGEDeposited(event: TGEDeposited): void {
+  let campaign = Campaign.load(event.params._campaignID.toHex());
+  if (campaign) {
+    campaign.IsTGE = true;
+    campaign.TgeAmount = event.params._tgeAmount;
+    campaign.TgeTime = event.params._tgeTime;
+    campaign.save();
+  }
+}
